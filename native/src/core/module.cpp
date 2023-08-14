9fd48738d57a7da51f5bff7830f43cbd864af3f7
Takeda-senpai <deadboltsx303@gmail.com>
2023-08-14 10:08:45 +0700

    add early-mount.d per module

--- native/src/core/module.cpp
+++ native/src/core/module.cpp
@@ -508,6 +508,8 @@ void disable_modules() {
         if (off) {
             ssprintf(buf + off, sizeof(buf) - off, "/%s/sepolicy.rule", entry->d_name);
             unlink(buf);
+            ssprintf(buf + off, sizeof(buf) - off, "/%s/early-mount", entry->d_name);
+            rm_rf(buf);
         }
     });
 }
@@ -522,6 +524,8 @@ void remove_modules() {
         if (off) {
             ssprintf(buf + off, sizeof(buf) - off, "/%s/sepolicy.rule", entry->d_name);
             unlink(buf);
+            ssprintf(buf + off, sizeof(buf) - off, "/%s/early-mount", entry->d_name);
+            rm_rf(buf);
         }
     });
     rm_rf(MODULEROOT);
